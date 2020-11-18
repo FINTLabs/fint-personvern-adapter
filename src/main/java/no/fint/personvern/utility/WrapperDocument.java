dfbbd0cce1f8fb03552968d70ae3366ab3eec6bd
Frode Sjovatsen <frode@fintlabs.no>
2020-11-18 23:39:11 +0100

    removed document annotation

--- src/main/java/no/fint/personvern/utility/WrapperDocument.java
+++ src/main/java/no/fint/personvern/utility/WrapperDocument.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
-@Document(collection = "personvern")
+//@Document(collection = "personvern")
 public class WrapperDocument {
 
     @Id
