1d370ecf11ce2ef7ebfdd4b0c65223b4457223ba
Trond Magnus Sevre <76692689+trondsevre@users.noreply.github.com>
2022-08-31 15:55:38 +0200

    Try without converters (#8)
    * Try without converters
    
    * Remove comment

--- src/main/java/no/fint/personvern/handler/samtykke/behandling/BehandlingEntity.java
+++ src/main/java/no/fint/personvern/handler/samtykke/behandling/BehandlingEntity.java
@@ -1,13 +1,16 @@
 package no.fint.personvern.handler.samtykke.behandling;
 
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import no.fint.model.resource.personvern.samtykke.BehandlingResource;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
 import javax.persistence.Column;
-import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
@@ -16,13 +19,16 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@TypeDefs({
+        @TypeDef(name = "json", typeClass = JsonType.class)
+})
 @Entity(name = "Behandling")
 public class BehandlingEntity {
 
     @Id
     private String id;
 
-    @Convert(converter = BehandlingConverter.class)
+    @Type(type = "json")
     @Column(columnDefinition = "json")
     private BehandlingResource resource;
 
