fe3f168b2fc20620e5c9f186e843f2c596742ec9
Hknots <Hknots99@gmail.com>
2023-04-13 15:30:37 +0200

    add event for samtykke

---
+++ src/main/java/no/fintlabs/personvern/samtykke/samtykke/event/SamtykkeEventPublisher.java
@@ -0,0 +1,70 @@
+package no.fintlabs.personvern.samtykke.samtykke.event;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import no.fint.model.resource.personvern.samtykke.SamtykkeResource;
+import no.fintlabs.adapter.config.AdapterProperties;
+import no.fintlabs.adapter.events.EventPublisher;
+import no.fintlabs.adapter.models.RequestFintEvent;
+import no.fintlabs.adapter.models.ResponseFintEvent;
+import no.fintlabs.adapter.models.SyncPageEntry;
+import no.fintlabs.personvern.samtykke.samtykke.SamtykkeRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@Service
+public class SamtykkeEventPublisher extends EventPublisher<SamtykkeResource> {
+
+    private final SamtykkeResourceValidator validator;
+
+    public SamtykkeEventPublisher(AdapterProperties adapterProperties, SamtykkeRepository repository, WebClient webClient, ObjectMapper objectMapper, SamtykkeResourceValidator validator) {
+        super("samtykke", SamtykkeResource.class, webClient, adapterProperties, repository, objectMapper);
+        this.validator = validator;
+    }
+
+    @Override
+    @Scheduled(initialDelayString = "9000", fixedDelayString = "60000")
+    public void doCheckForNewEvents() {
+        checkForNewEvents();
+    }
+
+    @Override
+    protected void handleEvent(RequestFintEvent requestFintEvent, SamtykkeResource samtykkeResource) {
+        ResponseFintEvent<SamtykkeResource> response = createResponse(requestFintEvent);
+
+        if (resourceNotValid(samtykkeResource, response)) return;
+
+        try {
+            SamtykkeResource updatedResource = repository.saveResources(samtykkeResource, requestFintEvent);
+            response.setValue(createSyncPageEntry(updatedResource));
+        } catch (Exception exception) {
+            response.setFailed(true);
+            response.setErrorMessage(exception.getMessage());
+            log.error("Error in repository.saveResource", exception);
+        }
+
+        submit(response);
+    }
+
+    protected SyncPageEntry<SamtykkeResource> createSyncPageEntry(SamtykkeResource resource) {
+        String identificationValue = resource.getSystemId().getIdentifikatorverdi();
+        return SyncPageEntry.of(identificationValue, resource);
+    }
+
+    private boolean resourceNotValid(SamtykkeResource samtykkeResource, ResponseFintEvent<SamtykkeResource> response) {
+
+        BeanPropertyBindingResult bindingResult = new BeanPropertyBindingResult(samtykkeResource, "samtykkeResource");
+        validator.validate(samtykkeResource, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            response.setRejected(true);
+            response.setRejectReason(bindingResult.toString());
+            submit(response);
+        }
+
+        return bindingResult.hasErrors();
+    }
+}
---
+++ src/main/java/no/fintlabs/personvern/samtykke/samtykke/event/SamtykkeEventSubscriber.java
@@ -0,0 +1,25 @@
+package no.fintlabs.personvern.samtykke.samtykke.event;
+
+import lombok.extern.slf4j.Slf4j;
+import no.fint.model.resource.personvern.samtykke.SamtykkeResource;
+import no.fintlabs.adapter.config.AdapterProperties;
+import no.fintlabs.adapter.events.EventSubscriber;
+import no.fintlabs.adapter.models.ResponseFintEvent;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@Service
+public class SamtykkeEventSubscriber extends EventSubscriber<SamtykkeResource, SamtykkeEventPublisher> {
+
+    protected SamtykkeEventSubscriber(WebClient webClient, AdapterProperties adapterProperties, SamtykkeEventPublisher publisher) {
+        super(webClient, adapterProperties, publisher, "samtykke");
+    }
+
+    @Override
+    protected void responsePostingEvent(ResponseEntity<Void> response, ResponseFintEvent<SamtykkeResource> responseFintEvent) {
+        log.info("Posting response for event {} returned {}.", responseFintEvent.getCorrId(), response.getStatusCode());
+    }
+
+}
