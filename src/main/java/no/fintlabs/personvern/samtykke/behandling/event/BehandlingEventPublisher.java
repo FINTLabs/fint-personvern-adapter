1771576c85f9615ee5941343f01737de480a0dc9
Henrik Knutsen <hknut99@gmail.com>
2023-06-14 14:31:43 +0200

    add systemId generation (#14)

--- src/main/java/no/fintlabs/personvern/samtykke/behandling/event/BehandlingEventPublisher.java
+++ src/main/java/no/fintlabs/personvern/samtykke/behandling/event/BehandlingEventPublisher.java
@@ -5,10 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import no.fint.model.resource.personvern.samtykke.BehandlingResource;
 import no.fintlabs.adapter.config.AdapterProperties;
 import no.fintlabs.adapter.events.EventPublisher;
+import no.fintlabs.adapter.models.OperationType;
 import no.fintlabs.adapter.models.RequestFintEvent;
 import no.fintlabs.adapter.models.ResponseFintEvent;
 import no.fintlabs.adapter.models.SyncPageEntry;
 import no.fintlabs.personvern.samtykke.behandling.BehandlingRepository;
+import no.fintlabs.utils.FintUtils;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,9 +19,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 public class BehandlingEventPublisher extends EventPublisher<BehandlingResource> {
 
+    private final FintUtils fintUtils;
 
-    public BehandlingEventPublisher(AdapterProperties adapterProperties, BehandlingRepository repository, WebClient webClient, ObjectMapper objectMapper) {
+    public BehandlingEventPublisher(AdapterProperties adapterProperties, BehandlingRepository repository, WebClient webClient, ObjectMapper objectMapper, FintUtils fintUtils) {
         super("behandling", BehandlingResource.class, webClient, adapterProperties, repository, objectMapper);
+        this.fintUtils = fintUtils;
     }
 
     @Override
@@ -33,6 +37,9 @@ public class BehandlingEventPublisher extends EventPublisher<BehandlingResource>
         ResponseFintEvent<BehandlingResource> response = createResponse(requestFintEvent);
 
         try {
+            if (requestFintEvent.getOperationType() == OperationType.CREATE) {
+                behandlingResource.setSystemId(fintUtils.createNewSystemId());
+            }
             BehandlingResource updatedResource = repository.saveResources(behandlingResource, requestFintEvent);
             response.setValue(createSyncPageEntry(updatedResource));
         } catch (Exception exception) {
--- src/main/java/no/fintlabs/personvern/samtykke/samtykke/event/SamtykkeEventPublisher.java
+++ src/main/java/no/fintlabs/personvern/samtykke/samtykke/event/SamtykkeEventPublisher.java
@@ -5,10 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import no.fint.model.resource.personvern.samtykke.SamtykkeResource;
 import no.fintlabs.adapter.config.AdapterProperties;
 import no.fintlabs.adapter.events.EventPublisher;
+import no.fintlabs.adapter.models.OperationType;
 import no.fintlabs.adapter.models.RequestFintEvent;
 import no.fintlabs.adapter.models.ResponseFintEvent;
 import no.fintlabs.adapter.models.SyncPageEntry;
 import no.fintlabs.personvern.samtykke.samtykke.SamtykkeRepository;
+import no.fintlabs.utils.FintUtils;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,8 +19,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 public class SamtykkeEventPublisher extends EventPublisher<SamtykkeResource> {
 
-    public SamtykkeEventPublisher(AdapterProperties adapterProperties, SamtykkeRepository repository, WebClient webClient, ObjectMapper objectMapper) {
+    private final FintUtils fintUtils;
+
+    public SamtykkeEventPublisher(AdapterProperties adapterProperties, SamtykkeRepository repository, WebClient webClient, ObjectMapper objectMapper, FintUtils fintUtils) {
         super("samtykke", SamtykkeResource.class, webClient, adapterProperties, repository, objectMapper);
+        this.fintUtils = fintUtils;
     }
 
     @Override
@@ -32,6 +37,9 @@ public class SamtykkeEventPublisher extends EventPublisher<SamtykkeResource> {
         ResponseFintEvent<SamtykkeResource> response = createResponse(requestFintEvent);
 
         try {
+            if (requestFintEvent.getOperationType() == OperationType.CREATE) {
+                samtykkeResource.setSystemId(fintUtils.createNewSystemId());
+            }
             SamtykkeResource updatedResource = repository.saveResources(samtykkeResource, requestFintEvent);
             response.setValue(createSyncPageEntry(updatedResource));
         } catch (Exception exception) {
